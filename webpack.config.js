677b47de1e617e598a1b520130575c0c80bfbf18
gabeyy <16877213+gabeyyplx@users.noreply.github.com>
2020-07-01 18:22:08 +0200

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -56,18 +56,6 @@ module.exports = {
         ],
       },
       {
-        test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: [
-          {
-            loader: "file-loader",
-            options: {
-              name: "[name].[ext]",
-              outputPath: "fonts/",
-            },
-          },
-        ],
-      },
-      {
         test: /\.(png|svg|jpg|gif)$/,
         use: [
           {
